bcda88509ab98646196eb59475d794e40e1b9e1e
peter.gu <peter.gu@infomax.com.tw>
2016-01-20 16:08:26 +0800

    Signed-off-by: peter.gu <peter.gu@infomax.com.tw>
    	modified:   MainThread.java

--- ThinkingJava/concurrency/MainThread.java
+++ ThinkingJava/concurrency/MainThread.java
@@ -1,6 +1,6 @@
 public class MainThread {
     public static void main(String[] args) {
-        LiftOff launch = new LiftOff();
+        LiftOff launch = new LiftOff(30);
         launch.run();
     }
 }
